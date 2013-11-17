6c3cd69098f357236c5455b7afb86bd23f85c82b
ss <git@push.com>
2013-11-17 00:30:23 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Instadata::Application.config.secret_token = '2e67c322db81a6432749af857c739657bc4aa6e2943f07b1df5c77bf9ec3148a7d7e42a745bd3b4b7f874c9c05cca049bdb365017bf5f5ad0ab68c076e550fe1'
